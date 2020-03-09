0c4d144f1bfa9fd08e185c2fd34146c9e5f76d14
Mustapha-Elzehary <61952094+Mustapha-Elzehary@users.noreply.github.com>
2020-03-09 02:49:58 +0200

    Add files via upload

---
+++ sec.py
@@ -0,0 +1,100 @@
+
+
+
+def getGeneratedKeyVig(word):
+    vigGlobalKey = "key"
+    generatedKey, currIndex = "", 0
+    for i in range(len(word)):
+        if (currIndex >= len(vigGlobalKey)):
+            currIndex = 0
+        generatedKey += vigGlobalKey[currIndex]
+        currIndex += 1
+
+
+def encVig(word):
+    encWord = ""
+    genKey = getGeneratedKeyVig(word)
+    word.upper()
+    for i in range(len(word)):
+        if (word[i].islower()):
+            encWord += chr(((ord(word[i].upper()) - 65) + (ord(genKey[i]) - 65)) % 26 + 65)
+        else:
+            print ((((ord(word[i]) - 65) + (ord(genKey[i]) - 65)) % 26 + 65))
+            encWord += chr(((ord(word[i]) - 65) + (ord(genKey[i]) - 65)) % 26 + 65)
+    return encWord
+
+def decVig(word):
+    decWord = ""
+    genKey = getGeneratedKeyVig(word)
+    for i in range(len(word)):
+        decWord += chr(((ord(word[i]) - 65) - (ord(genKey[i]) - 65) + 26) % 26 + 65)
+    return decWord
+
+
+def encAffine(word):
+    encWord, a, b = "", 17, 20
+    ''' 
+        C = (a*x + b) % 26 
+    '''
+    for i in range(len(word)):
+        if (word[i].islower()):
+            encWord += chr(((a * ord(word[i].upper()) - 65) + b) % 26 + 65)
+        else:
+            encWord += chr(((a * ord(word[i]) - 65) + b) % 26 + 65)
+
+    return encWord
+
+def decAffine(word):
+    decWord, a, b ,aInverse= "", 17, 20, 0
+
+    for i in range(26):
+        if ((a * i) % 26) == 1:
+            aInverse = i
+    ''' 
+       x = (a^-1 * (C - b)) % 26 
+    '''
+    for i in range(len(word)):
+        decWord += chr(((aInverse * (ord(word[i]) - b - 65)) % 26) + 65)
+    return decWord
+
+
+def encShift(input):
+    output, s = "", 4
+
+    for i in range(len(input)):
+
+        if (input[i].isupper()):
+            output += chr((ord(input[i]) + s - 65) % 26 + 65)
+        else:
+            output += chr((ord(input[i]) + s - 97) % 26 + 97)
+    return output
+
+
+def decShift(input):
+    output, s = "", 4
+    for i in range(len(input)):
+
+        if (input[i].isupper()):
+            output += chr((ord(input[i]) - s - 65) % 26 + 65)
+        else:
+            output += chr((ord(input[i]) - s - 97) % 26 + 97)
+    return output
+
+
+
+
+affineWord =encAffine("MOSTAFA")
+print (affineWord)
+print(decAffine(affineWord))
+print ("------------------------------\n")
+
+Word =encShift("MOSTAFA")
+print (Word)
+print(decShift(Word))
+print ("------------------------------\n")
+
+
+Word =encVig("MOSTAFA")
+print (Word)
+print(decVig(Word))
+print ("------------------------------\n")
